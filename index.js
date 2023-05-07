e4df244a5831ebf79b9f913d8e5606d7efdf7c02
Kenneth Lee <kennethlee@Kenneths-MacBook-Pro.local>
2023-05-06 18:40:47 -0700

    uibuild

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ require("dotenv").config();
 app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, "build")));
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
+});
 // console.log("hi");
 
 mongoose
@@ -47,9 +49,6 @@ app.all("*", (req, res, next) => {
   res.status(404).json({ error: "unknown endpoint" });
 });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/build/index.html"));
-});
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
