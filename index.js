ec05e32fb00d7618b130b0dbd7511805cc41b4a3
Kenneth Lee <kennethlee@Kenneths-MBP.attlocal.net>
2023-05-09 02:57:42 -0700

    uibuild

--- index.js
+++ index.js
@@ -15,9 +15,23 @@ app.get("*", (req, res) => {
 });
 // console.log("hi");
 
+const enforceHTTPS = (req, res, next) => {
+  if (req.headers["x-forwarded-proto"] !== "https") {
+    return res.redirect(["https://", req.get("Host"), req.url].join(""));
+  }
+  next();
+};
+const redirectToNonWWW = (req, res, next) => {
+  if (req.headers.host.slice(0, 4) === "www.") {
+    const nonWWWHost = req.headers.host.slice(4);
+    return res.redirect(["https://", nonWWWHost, req.url].join(""));
+  }
+  next();
+};
+
 if (process.env.NODE_ENV === "production") {
-  app.use(middleware.enforceHTTPS);
-  app.use(middleware.redirectToNonWWW);
+  app.use(enforceHTTPS);
+  app.use(redirectToNonWWW);
 }
 
 mongoose
