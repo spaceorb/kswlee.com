e5a49677480f9b9b73c030d5f36432751a9a18ff
spaceorb <kennethlee@Kenneths-MacBook-Pro.local>
2023-05-11 16:49:32 -0700

    uibuild

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ app.use(express.static(path.join(__dirname, "build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/build/index.html"));
 });
+console.log("process.env.NODE_ENV", process.env.NODE_ENV);
 if (process.env.NODE_ENV === "production") {
+  console.log("middleware.enforceHTTPS", middleware.enforceHTTPS);
+
   app.use(middleware.enforceHTTPS);
   app.use(middleware.redirectToNonWWW);
 }
