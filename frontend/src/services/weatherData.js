84d456ce0e21e215a413930cfa8a1ac5e41e6e60
Kenneth Lee <84689781+spaceorb@users.noreply.github.com>
2023-05-11 02:18:10 -0700

    Update weatherData.js

--- frontend/src/services/weatherData.js
+++ frontend/src/services/weatherData.js
@@ -1,23 +1,14 @@
 import axios from "axios";
-
 const URL = `https://api.weatherapi.com/v1/current.json?key=7ad25b8583224ebc8da133056230603&q=95035`;
-// const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
-// const URL = `https://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=95035`;
-// console.log("WEATHER_API_KEY", WEATHER_API_KEY);
-// console.log("URL", URL);
 
 const getWeather = async () => {
   try {
     const request = await axios.get(URL);
-    console.log("request", request);
-
-    console.log("zzzz");
     return request.data;
   } catch (error) {
-    console.error("hi", error);
+    console.error(error);
     return null;
   }
 };
 
-/* eslint-disable */
 export default { getWeather };
